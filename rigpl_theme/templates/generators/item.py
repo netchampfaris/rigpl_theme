444e746295cbcaf7bc2065ae61098b232049b80b
Faris Ansari <netchamp.faris@gmail.com>
2018-10-12 17:10:48 +0530

    Skip attributes with only 1 value

--- rigpl_theme/templates/generators/item.py
+++ rigpl_theme/templates/generators/item.py
@@ -3,11 +3,15 @@ from itertools import product
 def get_context(context):
 	print(context.attributes, context.attribute_values)
 
+	valid_attribute_keys = []
 	values_to_cross = []
 
 	for d in context.attributes:
 		attr_values = context.attribute_values[d.attribute]
-		values_to_cross.append(attr_values)
+		if len(attr_values) > 1:
+			valid_attribute_keys.append(d.attribute)
+			values_to_cross.append(attr_values)
 
 	context.cross_product = list(product(*values_to_cross))
+	context.valid_attribute_keys = valid_attribute_keys
 	return context
